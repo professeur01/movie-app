08fa1e45f9c46760c2f791acab35985b44ceab50
Awal Massaoudou <awaloumassaoudou@gmail.com>
2024-03-26 07:47:06 +0100

    commit

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { View, Text,Image, ScrollView, Button } from "react-native";
-const imgAdaptive = require("./assets/adaptive-icon.png");
+import { View, Text,Image, ScrollView, Pressable, Button } from "react-native";
+const imgAvatar = require("./assets/img/Learning.gif");
 
 export default function App() {
   return (
-    <View style={{ flex: "1", backgroundColor: "plum", padding: 40 }}>
-    <Button title="Press" color={"white"} disabled onPress={()=> console.log("Button Pressed")}/>
+    <View style={{ flex: "1",justifyContent:"space-around",alignItems:"center", backgroundColor: "white", padding: 40 }}>
+   <Image source={imgAvatar} style={{width:350, height:350}}></Image>
+   <Button title="Click to continue" onPress={()=>console.log("my first welcome page in mobile developement")}/>
     </View>
   );
 }
