64eb57717bd7fcb0e6efb63c4968da4be89bfd5b
Awal Massaoudou <awaloumassaoudou@gmail.com>
2024-05-01 00:54:25 +0100

    commit

--- App.js
+++ App.js
@@ -1,55 +1,19 @@
-import { useState } from "react";
-import {
-  View,
-  Text,
-  Image,
-  ScrollView,
-  Pressable,
-  Button,
-  Modal,
-  StyleSheet
-} from "react-native";
-const imgAvatar = require("./assets/img/Learning.gif");
-
+import { View, Text, StyleSheet, ImageBackground } from "react-native";
+const chat_haut_wp = require("./assets/chat_haut_wp.jpeg");
 export default function App() {
-  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
-    <View
-      style={{
-        flex: "1",
-        justifyContent: "space-around",
-        alignItems: "center",
-        backgroundColor: "white",
-        padding: 40,
-      }}
-    >
-      <Image source={imgAvatar} style={{ width: 350, height: 350 }}></Image>
-      <Button
-        title="Click to continue"
-        onPress={() => setIsModalVisible(true)}
-      />
-      <Modal
-        visible={isModalVisible}
-        onRequestClose={() => setIsModalVisible(false)}
-        animationType="slide"
-        presentationStyle="pageSheet"
-      >
-        <View style={styles.modalSyle}>
-          <Text>This is a modal!</Text>
-        </View>
-        <Button title="Close Modal" onPress={() => setIsModalVisible(false)} />
-      </Modal>
-    </View>
+  <ImageBackground
+  source={chat_haut_wp}
+  style={styles.background}>
+
+  </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  modalSyle: {
-   flex:1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "green",
-    padding: 60,
-  },
- 
-});
+  background:{
+    flex: 1,
+  }
+})
+
+
